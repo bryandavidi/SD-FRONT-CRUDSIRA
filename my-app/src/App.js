54a1270619d09a3f6a1217d512053d147e1f678f
David <bryan.ibanez@uptc.edu.co>
2022-10-27 00:15:02 -0500

    generacion de rutas de navegacion

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,33 @@
 import React,{ useState, useEffect} from "react";
+import { Route, Routes } from "react-router-dom";
 import CourseList from "./Components/courseList.js";
 import InscriptionList from "./Components/inscriptionList.js";
 import StudentList from "./Components/studentList.js";
 
-function App(){
+const Login = () => <h1>Login</h1>
+const Home = () => <h1>Home</h1>
 
-    const [courses,setcourses] = useState([])
-    const [inscriptions,setinscription] = useState([])
+const Student = () => {
     const [students,setstudent] = useState([])
 
-
-
-    useEffect(() =>{
-        const getCourse = () => {
-            fetch('http://localhost:3000/materias/todas')
+    useEffect(()=>{
+        const getstudent = () => {
+            fetch('http://localhost:3000/estudiantes/todos')
             .then(res => res.json())
-            .then(res => setcourses(res))
-            
+            .then(res => setstudent(res))
         }
-        getCourse()
+        getstudent()
+    },[]);
 
+    return (
+    <StudentList students={students}/>
+    );
+}
+
+const  Inscription = () => {
+    const [inscriptions,setinscription] = useState([])
+
+    useEffect(()=>{
         const getinscription = () => {
             fetch('http://localhost:3000/inscripciones/todas')
             .then(res => res.json())
@@ -27,27 +35,41 @@ function App(){
             
         }
         getinscription()
-
-        const getstudent = () => {
-            fetch('http://localhost:3000/estudiantes/todos')
-            .then(res => res.json())
-            .then(res => setstudent(res))
-            
-        }
-        getstudent()
-    }, [])
+    },[]);
 
     return(
-        <div className="container">
-            <div className="row">
-                <h2 style={{textAling: 'center'}}>LISTA DE CURSOS</h2>
-                <CourseList courses={courses}/>
-                <h2 style={{textAling: 'center'}}>LISTA DE INSCRIPCIONES</h2>
-                <InscriptionList inscriptions={inscriptions}/>
-                <h2 style={{textAling: 'center'}}>LISTA DE ESTUDIANTES</h2>
-                <StudentList students={students}/>
+        <InscriptionList inscriptions={inscriptions}/>
+    );
+}
+
+const Course = () => {
+    const [courses,setcourses] = useState([])
+
+    useEffect(()=>{
+        const getCourse = () => {
+            fetch('http://localhost:3000/materias/todas')
+            .then(res => res.json())
+            .then(res => setcourses(res))
             
-            </div>
+        }
+        getCourse()
+    },[]);
+    
+    return(
+        <CourseList courses={courses}/>
+    );
+}
+
+function App(){
+    return(
+        <div className="container">
+            <Routes>
+                <Route path="/sesion" element ={<Login />}/>
+                <Route path="/inicio" element ={<Home />}/>
+                <Route path="/estudiantes" element ={<Student />}/>
+                <Route path="/materias" element ={<Course />}/>
+                <Route path="/inscripciones" element ={<Inscription />}/>
+            </Routes>
         </div>
     );
 }
