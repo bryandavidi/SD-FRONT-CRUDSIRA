e8b988b9552fb0711974a5f7a04a930277c1b86f
David <bryan.ibanez@uptc.edu.co>
2022-10-27 19:09:31 -0500

    Creacion de rutas iniciales para los botones del navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,73 +1,73 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
 import {Route, Routes } from "react-router-dom";
-// import CourseList from "./Components/CourseList.js";
-// import InscriptionList from "./Components/InscriptionList.js";
-// import StudentList from "./Components/StudentList.js";
+import CourseList from "./Components/CourseList.js";
+import InscriptionList from "./Components/InscriptionList.js";
+import StudentList from "./Components/StudentList.js";
 import Login from "./Components/Login.js";
-import NavBarHome from "./layouts/navbar.js";
+import NavBarHome from "./layouts/Navbar.js";
 
-// const Student = () => {
-//     const [students,setstudent] = useState([])
+const Student = () => {
+    const [students,setstudent] = useState([])
 
-//     useEffect(()=>{
-//         const getstudent = () => {
-//             fetch('http://localhost:3000/estudiantes/todos')
-//             .then(res => res.json())
-//             .then(res => setstudent(res))
-//         }
-//         getstudent()
-//     },[]);
+    useEffect(()=>{
+        const getstudent = () => {
+            fetch('http://localhost:3000/estudiantes/todos')
+            .then(res => res.json())
+            .then(res => setstudent(res))
+        }
+        getstudent()
+    },[]);
 
-//     return (
-//     <StudentList students={students}/>
-//     );
-// }
+    return (
+    <StudentList students={students}/>
+    );
+}
 
-// const  Inscription = () => {
-//     const [inscriptions,setinscription] = useState([])
+const  Inscription = () => {
+    const [inscriptions,setinscription] = useState([])
 
-//     useEffect(()=>{
-//         const getinscription = () => {
-//             fetch('http://localhost:3000/inscripciones/todas')
-//             .then(res => res.json())
-//             .then(res => setinscription(res))
+    useEffect(()=>{
+        const getinscription = () => {
+            fetch('http://localhost:3000/inscripciones/todas')
+            .then(res => res.json())
+            .then(res => setinscription(res))
             
-//         }
-//         getinscription()
-//     },[]);
+        }
+        getinscription()
+    },[]);
 
-//     return(
-//         <InscriptionList inscriptions={inscriptions}/>
-//     );
-// }
+    return(
+        <InscriptionList inscriptions={inscriptions}/>
+    );
+}
 
-// const Course = () => {
-//     const [courses,setcourses] = useState([])
+const Course = () => {
+    const [courses,setcourses] = useState([])
 
-//     useEffect(()=>{
-//         const getCourse = () => {
-//             fetch('http://localhost:3000/materias/todas')
-//             .then(res => res.json())
-//             .then(res => setcourses(res))
+    useEffect(()=>{
+        const getCourse = () => {
+            fetch('http://localhost:3000/materias/todas')
+            .then(res => res.json())
+            .then(res => setcourses(res))
             
-//         }
-//         getCourse()
-//     },[]);
+        }
+        getCourse()
+    },[]);
     
-//     return(
-//         <CourseList courses={courses}/>
-//     );
-// }
+    return(
+        <CourseList courses={courses}/>
+    );
+}
 
 function App(){
     return(
         <Routes>
-                <Route path="/login" element ={<Login />}/>
-                <Route path="/home" element ={<NavBarHome />}/>
-                {/* <Route path="/inicio" element ={<Home />}/>
-                <Route path="/estudiantes" element ={<Student />}/>
-                <Route path="/materias" element ={<Course />}/>
-                <Route path="/inscripciones" element ={<Inscription />}/> */}
+                <Route path = "/menu" element ={<NavBarHome />}>
+                    <Route path = "estudiantes" element ={<Student />}/>
+                    <Route path = "materias" element ={<Course />}/>
+                    <Route path = "inscripciones" element ={<Inscription />}/>
+                </Route>
+                <Route path = "login" element ={<Login />}/>
         </Routes>      
     );
 }
--- my-app/src/layouts/navbar.js
+++ my-app/src/layouts/navbar.js
@@ -3,35 +3,39 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Outlet, Link } from 'react-router-dom';
 
 function NavBarHome (){
     return(
-    <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
-      <Container>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <NavDropdown title="Estudiantes" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/1.1">Mostrar estudiantes</NavDropdown.Item>
-              <NavDropdown.Item href="#action/1.2">Crear estudiante</NavDropdown.Item>
-            </NavDropdown>
+        <div>
+         <Navbar collapseOnSelect expand="sm" bg="primary" variant="dark">
+            <Container>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                    <NavDropdown title="Estudiantes" id="collasible-nav-dropdown">
+                    <NavDropdown.Item as={Link} to='estudiantes' >Mostrar estudiantes</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/1.2">Crear estudiante</NavDropdown.Item>
+                    </NavDropdown>
 
-            <NavDropdown title="Materias" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/2.1">Mostrar materias</NavDropdown.Item>
-              <NavDropdown.Item href="#action/2.2">Crear materia</NavDropdown.Item>
-            </NavDropdown>
+                    <NavDropdown title="Materias" id="collasible-nav-dropdown">
+                    <NavDropdown.Item as={Link} to = 'materias'>Mostrar materias</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/2.2">Crear materia</NavDropdown.Item>
+                    </NavDropdown>
 
-            <NavDropdown title="Inscripciones" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Mostrar Inscripciones</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Generar Inscripcion</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#salir">Cerrar sesión</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+                    <NavDropdown title="Inscripciones" id="collasible-nav-dropdown">
+                    <NavDropdown.Item as={Link} to='inscripciones'>Mostrar Inscripciones</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Generar Inscripcion</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                <Nav>
+                    <Nav.Link as={Link} to='/login'>Cerrar sesión</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        <Outlet/>
+        </div>
     );
 }
 
